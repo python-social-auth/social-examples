8fa7c8f1b51f26ef8c7bd871f0c035e8dd6505af
Matias Aguirre <matiasaguirre@gmail.com>
2017-03-25 10:02:07 -0300

    Update mail validation function to use new partial pipeline

--- example-django-mongoengine/app/mail.py
+++ example-django-mongoengine/app/mail.py
@@ -3,10 +3,11 @@ from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 
 
-def send_validation(strategy, backend, code):
-    url = '{0}?verification_code={1}'.format(
+def send_validation(strategy, backend, code, partial_token):
+    url = '{0}?verification_code={1}&partial_token={2}'.format(
         reverse('social:complete', args=(backend.name,)),
-        code.code
+        code.code,
+        partial_token
     )
     url = strategy.request.build_absolute_uri(url)
     send_mail('Validate your account', 'Validate your account {0}'.format(url),
